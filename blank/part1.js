55932519fd0ed4c6c6795751b8d68b5ef1c74a8c
Saswat Mahapatra <92862073+logan1o1@users.noreply.github.com>
2023-10-09 05:15:32 +0000

    datatypes

--- blank/part1.js
+++ blank/part1.js
@@ -1,1 +1,24 @@
-console.log("clearing the fundamental and going indepth")
+"use strict";
+const accId = 12345;
+let accMail = "lucifer666@gmail.com";
+var accPassword = "123456";
+let accCity ="Cuttack";
+let accState;
+
+accMail = "Asura@gmail.com";
+accPassword = "0987654";
+accCity = "Venice"
+
+// console.table([accId,accMail,accPassword,accCity]);
+
+let score = 20
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// 20 => number
+// "abc" => string
+// "20qwe" => NaN; but it's type is number
+// unidentified => NaN ^
+// null => 0: type is object
+// true => 1; false => 0
