12021d3a98e3bde4739b3c17e0a0bde3a15af4ce
Saswat Mahapatra <92862073+logan1o1@users.noreply.github.com>
2023-10-09 13:13:41 +0000

    maths n shit

--- blank/part1.js
+++ blank/part1.js
@@ -1,17 +1,17 @@
 "use strict";
-const accId = 12345;
-let accMail = "lucifer666@gmail.com";
-var accPassword = "123456";
-let accCity ="Cuttack";
-let accState;
+// const accId = 12345;
+// let accMail = "lucifer666@gmail.com";
+// var accPassword = "123456";
+// let accCity ="Cuttack";
+// let accState;
 
-accMail = "Asura@gmail.com";
-accPassword = "0987654";
-accCity = "Venice"
+// accMail = "Asura@gmail.com";
+// accPassword = "0987654";
+// accCity = "Venice"
 
 // console.table([accId,accMail,accPassword,accCity]);
 
-let score = 20
+// let score = 20
 
 // console.log(typeof score);
 // console.log(typeof(score));
@@ -52,20 +52,53 @@ let score = 20
 
 // non primitive data types (arrays, objects, functions)
 
- const myFub =function () {
-    console.log("Fuck you");
-}
+//  const myFub =function () {
+//     console.log("Fuck you");
+// }
 
-const hotties = ["mila kunis", "emma stones", "emma mayers"];
+// const hotties = ["mila kunis", "emma stones", "emma mayers"];
 
-let myObj1 = {
-    name: "Lucifer",
-    age: 1000
-};
+// let myObj1 = {
+//     name: "Lucifer",
+//     age: 1000
+// };
 
-console.log(typeof myObj1);
+// console.log(typeof myObj1);
 
 // non primitive data types return object or function as data types
 
-// Stack(primitive) when we change an existing variable it gives us a copy of the original i,e the original's value is not changed, 
+// Stack(primitive) when we change an existing variable it gives us a copy of the original i,e the original's value is not changed
 // Heap(non-primitive) when we change an existing variable it gives us a reference of the original i,e original's value changes
+
+// when doin strings use backticks ``
+
+// let name = "Pietro";
+// let age = 70
+// console.log(`My name is ${name} and my age is ${age}`);
+
+// const gameName = new String("arguments");
+
+// console.log(gameName[3]);
+// console.log(gameName.__proto__);
+// console.log(typeof gameName);
+
+// const url = "http://fuck%20you.wiki/bhen%20k%20laude";
+// console.log(url.replace(/%20/g, '-'));
+
+// const newNum = new Number(134)
+// console.log(newNum.toFixed(3));
+
+// let n1 = 34.8556
+// console.log(n1.toPrecision(3));
+
+
+// **********************  Maths ************************
+
+// console.log(Math);
+
+// console.log(Math.floor(Math.random()*10)+1);
+
+let max = 6;
+let min = 1;
+
+console.log(Math.floor(Math.random()*(max-min+1)));
