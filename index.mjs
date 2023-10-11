e61c24df37f907be0529597044dd14c6bfdecae0
Saswat Mahapatra <92862073+logan1o1@users.noreply.github.com>
2023-10-11 12:33:18 +0000

    delete

--- index.mjs
+++
@@ -1,7 +0,0 @@
-let a =30;
-let b= 40;
-let c= 50;
-
-export default a;
-export {b};
-export {c};
--- indx2.mjs
+++
@@ -1,4 +0,0 @@
-import a, {b,c} from './index.mjs';
-console.log(a)
-console.log(b)
-console.log(c)
